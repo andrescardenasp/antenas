136400bd8cdf27a8a622fcc736cd12433b4859c2
AndresCardenas <andrescardenasp@yahoo.com>
2018-08-03 23:00:43 +0200

    first working code

--- src/main/scala/load.scala
+++ src/main/scala/load.scala
@@ -1,14 +1,23 @@
+import org.apache.spark._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.sql.SQLContext
+import scala.collection.JavaConversions._
 
-//import org.apache.log4j.Logger
-//import org.apache.log4j.Level
-//import org.apache.spark.sql.SQLContext
+
+object load {
+  //  val ages = Seq(42, 75, 29, 64)
+  //  println(s"The oldest person is ${ages.max}")
+
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("Antenas-Monetizacion").setMaster("local")
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = new SQLContext(sc)
+
+    val textFile2 = sc.textFile("hdfs://localhost:9000/input/cities-01_06_2017.csv")
+
+    textFile2.collect().foreach(println)
 
 
-
-object load extends App {
-  val ages = Seq(42, 75, 29, 64)
-  println(s"The oldest person is ${ages.max}")
-
-  //val textFile = sc.textFile("hdfs://...")
-
+  }
 }
