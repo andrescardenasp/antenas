ce52217c46c355b5d6991d7bcebcb71ce0d3c4a6
AndresCardenas <andrescardenasp@yahoo.com>
2018-08-03 20:46:44 +0200

    adding dependencies

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "antenas"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.3.1"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.3.1"
+
---
+++ src/main/scala/load.scala
@@ -0,0 +1,14 @@
+
+//import org.apache.log4j.Logger
+//import org.apache.log4j.Level
+//import org.apache.spark.sql.SQLContext
+
+
+
+object load extends App {
+  val ages = Seq(42, 75, 29, 64)
+  println(s"The oldest person is ${ages.max}")
+
+  //val textFile = sc.textFile("hdfs://...")
+
+}
